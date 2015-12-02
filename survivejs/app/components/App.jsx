5cb1fc3463ab1299eca3f65a5a231d1d5138de11
Michael Leow <leow@sinarproject.org>
2015-12-02 16:02:20 +0800

    Summary: Notes and Flux (still broken) :(

--- survivejs/app/components/App.jsx
+++ survivejs/app/components/App.jsx
@@ -1,37 +1,29 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = NoteStore.getState();
-    }
-
-    componentDidMount() {
-        NoteStore.listen(this.storeChanged);
-    }
-
-    componentWillUnmount() {
-        NoteStore.unlisten(this.storeChanged);
-    }
-
-    storeChanged = (state) => {
-        this.setState(state);
-    }
 
     render() {
-        const notes = this.state.notes;
-
         return (
             <div>
                 <button className="add-note" onClick={this.addNote}>+
                 </button>
-                <Notes items={notes}
-                       onEdit={this.editNote}
-                       onDelete={this.deleteNote}
-                />
+                <AltContainer
+                    stores={[NoteStore]}
+                    inject={
+                        {
+                            items: () => NoteStore.getState().notes
+                        }
+                    }
+                >
+                    <Notes items={notes}
+                           onEdit={this.editNote}
+                           onDelete={this.deleteNote}
+                    />
+                </AltContainer>
             </div>
         );
     }
