9b6a1424a9fc2b5ecf68ce67cb58c236ae6bfe33
Michael Leow <leow@sinarproject.org>
2016-04-27 21:55:17 +0800

    Summary: Loading data for PAR from MapIt data
    Changesets:
    - Add MAPIT init scripts for the folders/collections/scripts (including truncate js)
    - Add script to refresh PAR MapIt with the index and each details
    - Needed to handle when hitting rate limit on MapIt servers; and feature
    - Try out AQL to JOIN and gather the MapIt data GeoJSON based on PAR Code
    - TODO: Need to explore why the arangosh execute-string did not seem to work, but works in file; to try on the Linux box in c9.io to see

--- openspending-lite/data/ARANGOJS/setup-base-db-collections.js
+++ openspending-lite/data/ARANGOJS/setup-base-db-collections.js
@@ -1,12 +1,26 @@
-console.log("Creating new Database sinar ...")
+// Console output like below does not seem to wirk :(
+// console.log("Creating new Database sinar ...")
 db._createDatabase("sinar")
 db._useDatabase("sinar")
-console.log("Creating Document Collection named ... ")
+// console.log("Creating Document Collection named ... ")
 db._createDocumentCollection("posts")
 db._createDocumentCollection("persons")
 db._createDocumentCollection("organizations")
 db._createDocumentCollection("memberships")
 
+// Create for popit
+db._createDocumentCollection("popit_posts")
+db._createDocumentCollection("popit_persons")
+db._createDocumentCollection("popit_organizations")
+db._createDocumentCollection("popit_memberships")
+
+// Create for mapit
+db._createDocumentCollection("par")
+db._createDocumentCollection("par_details")
+db._createDocumentCollection("dun")
+db._createDocumentCollection("dun_details")
+db._createDocumentCollection("zon")
+db._createDocumentCollection("zon_details")
 // Make above more robust by checking if they exist
 // Can print out console outputs??
 // TODO: Create the needed index; possibly as separate helper files??functions ..
--- openspending-lite/data/SCRIPTPACK/1-setup-mapit.bash
+++ openspending-lite/data/SCRIPTPACK/1-setup-mapit.bash
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+#
+
+MYDATADIR=`pwd`
+
+echo "I am in ${MYDATADIR}"
+
+mkdir -p ${MYDATADIR}/MAPIT/{PAR,DUN,ZON}
+
