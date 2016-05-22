8db6fa9a7b8bf2b9ef5bff6cb5360dd40cef6791
Michael Leow <leow@sinarproject.org>
2016-05-23 05:56:15 +0800

    Summary: PoC of Composition with ArangoDB Implementation
    Changesets:
    - PoC of working Stamp with AQL and Promise
    - Tested and show passing args into Stamp init
    - Unable to get fully working with Promise out in the main function
    - Had to use .bind to get the context inside the Promise

--- openspending-lite/src/libs/repo-arangodb.js
+++ openspending-lite/src/libs/repo-arangodb.js
@@ -5,29 +5,72 @@
 
 import util from 'util'
 import stampit from 'stampit'
+import arangojs from 'arangojs'
 import repoInterface from './repo-base'
 
 const repoArangoDB = stampit
     .props({
-            database: "sinar",
+            type: "arangodb",
             collectionName: "flll-er-up"
         }
-    ).init(function () {
+    ).init(function ({instance, args}) {
             // Init the ArangoDB connection
+            instance.collectionName = args[0]
             // Using th db config ...
-            this.dbconn = "BOB"
+            this.dbconn = new arangojs.Database({
+                url: `http://${instance.db.host}:${instance.db.port}`
+            })
+            // DEBUG:
+            // this.log("instance %s args %s", util.inspect(instance), util.inspect(args))
+            // Switch to the db config name
+            this.dbconn.useDatabase(`${instance.db.name}`)
         }
     ).methods({
-            _promiseQueryAllArangoRepo (aql_query = null, bind_vars = null, options = null) {
-                // Execute async stuff here ..
-                // with needed bindings??
-                if (this.dbconn == null) {
-                    this.err("No change when init!!; is NULL!!!")
-                } else if (this.dbconn == undefined) {
-                    this.err("Changed; but still need to implement; is UNDEFINED!!!")
-                } else {
-                    this.log(util.inspect(this.dbconn))
-                }
+            _promiseQueryAllArangoRepo: function (aql_query = null, bind_vars = null, options = null) {
+
+                // console.log("CONTEXT", util.inspect(this))
+                return new Promise(function (resolve, reject) {
+
+                    // DEBUG:
+                    // this.err("DBCONN", util.inspect(this.dbconn))
+                    // Execute async stuff here ..
+                    // with needed bindings??
+                    if ((this.dbconn == null) || (this.dbconn == undefined)) {
+                        // DEBUG:
+                        this.err("No change when init!!; is NULL!!!")
+                        reject("No dbconn too!!")
+                        return
+                    }
+
+                    // GET this far is ok
+                    // this.log("ARANGODB", util.inspect(this.dbconn))
+
+                    this.dbconn.query(aql_query, bind_vars, options).then(
+                        cursor => {
+                            this.log(cursor.count, " result(s) returned!!")
+                            // self.result = cursor.count
+                            return cursor.all()
+                        },
+                        err => {
+                            this.err("ERR: ", err.code)
+                            reject(err.code)
+                        }
+                    ).then(
+                        value => {
+                            // console.log("Got back from ARANGODB: ", util.inspect(value))
+                            // DEBUG:
+                            // console.log(JSON.stringify(value))
+                            resolve(value)
+                        },
+                        err => {
+                            this.err("ERR: ", err.code)
+                            reject(err.code)
+                        }
+                    ).catch(
+                        err => reject(err.code)
+                    )
+                }.bind(this))
+
             },
             save () {
                 this.log("ArangoDB implementation of SAVE!!!")
@@ -40,7 +83,7 @@ const repoArangoDB = stampit
             dbconn: null
         }
     )
-
+  
 
 export default stampit.compose(repoInterface, repoArangoDB)
 
--- openspending-lite/src/poc.js
+++ openspending-lite/src/poc.js
@@ -1,6 +1,7 @@
 "use strict"
 
 import util from 'util'
+import config from './config'
 import repoArangoDB from './libs/repo-arangodb'
 
 //
@@ -9,7 +10,24 @@ import repoArangoDB from './libs/repo-arangodb'
 console.log("This is cool!! PoC!!!")
 // console.error("In PoC!!!", util.inspect(process))
 
-const repo = repoArangoDB()
+const repo = repoArangoDB(config, "aduanku")
+// const repo = repoArangoDB({}, "aduanku")
 repo.save()
 repo.query()
-repo._promiseQueryAllArangoRepo()
+let query = `
+    FOR a IN aduanku
+        LIMIT 0,2
+        RETURN a
+    `
+// let p = repo._promiseQueryAllArangoRepo(query, {}, {count: true})
+let p = repo._promiseQueryAllArangoRepo(query, {}, {})
+// DEBUG:
+console.log(util.inspect(p))
+p.then(
+    value => repo.log("PROMISE:", value)
+).catch(
+    err => repo.err()
+)
+
+
+
