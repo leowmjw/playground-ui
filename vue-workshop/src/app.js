91c1e7c07b5ab3bb5435530b6656bb509fba11e2
Michael Leow <leow@sinarproject.org>
2017-04-01 14:58:57 +0800

    Summary: First step; HTML
    Changesets:
    - Base template downloaded
    - Got the Avengers card covers
    - Ugly first cut HTML
    TODO: Think about the CSS styling ..

---
+++ vue-workshop/src/app.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    // mounted to <div id="app">
+    el: '',
+    // all data we need in UI
+    data: {
+        
+    },
+    // lifecycle, when component created
+    created: function () {
+        
+    },
+    // all data for compute purpose only, no logic
+    computed: {
+    },
+    // function to format display data in ui, same as angularjs filter
+    filters: {
+    },
+    // all the functions
+    methods: {
+    }
+});
